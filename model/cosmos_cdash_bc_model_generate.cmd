c99a7adcab6adfec3f910ff61fdc83b07e3b1dd1
Lex Jansen <lexjansen@users.noreply.github.com>
2022-12-14 16:44:02 -0500

    Initial CDASH model

---
+++ model/cosmos_cdash_bc_model_generate.cmd
@@ -0,0 +1,6 @@
+call ..\venv\Scripts\activate
+
+call gen-python cosmos_cdash_bc_model.yaml > cosmos_cdash_bc_model.py
+call gen-yuml cosmos_cdash_bc_model.yaml > cosmos_cdash_bc_model.yuml
+call gen-json-schema .\cosmos_cdash_bc_model.yaml > cosmos_cdash_bc_model.json
+call python .\create_svg.py .\cosmos_cdash_bc_model.yaml .\cosmos_cdash_bc_model.svg
