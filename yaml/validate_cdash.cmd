4e4a26fb4cec6efc02001f7dd5872d5ce02f2e31
Lex Jansen <lexjansen@users.noreply.github.com>
2022-12-15 09:30:34 -0500

    CDASH YAML utils

---
+++ yaml/validate_cdash.cmd
@@ -0,0 +1,35 @@
+@echo off
+cd %~dp0
+
+if /i (a%1) == (a) (REM
+     set /p package=Package ^[yyyymmdd^]: 
+     ) else (
+     set package=%1
+)
+
+if not exist "%~dp0\%package%\cdash\." (
+    echo Folder %~dp0%package%\cdash does not exist.
+    goto :EOF
+    )
+
+
+call ..\venv\Scripts\activate
+
+set logfile=%~dpn0_%package%.log
+echo.>%logfile%
+
+set counter=0
+for /F "eol=; tokens=1 delims=" %%i IN ('dir /b /s %~dp0%package%\cdash\*.yaml') do @call :ValidateFile %%i
+
+findstr /i /s "error" %logfile%
+goto:EOF
+
+:ValidateFile
+
+  set /a counter=counter+1 > nul
+  echo %counter% *** %1
+  echo *** %1 >> %logfile%
+
+  linkml-validate -C CDASHGroup -s ../model/cosmos_cdash_bc_model.yaml %1 >> %logfile% 2>>&1
+
+goto :EOF
