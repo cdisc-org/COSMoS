66961134df5cadd2a3522c2b65dd51b2e67bccab
Lex Jansen <lexjansen@gmail.com>
2023-02-16 12:17:53 -0500

    Add Mermaid erdiagrams

--- model/cosmos_bc_model_generate.cmd
+++ model/cosmos_bc_model_generate.cmd
@@ -2,5 +2,6 @@ call ..\venv\Scripts\activate
 
 call gen-python cosmos_bc_model.yaml > cosmos_bc_model.py
 call gen-yuml cosmos_bc_model.yaml > cosmos_bc_model.yuml
+call gen-erdiagram -no-structural cosmos_bc_model.yaml > cosmos_bc_model.md
 call gen-json-schema .\cosmos_bc_model.yaml > cosmos_bc_model.json
 call python .\create_svg.py .\cosmos_bc_model.yaml .\cosmos_bc_model.svg
